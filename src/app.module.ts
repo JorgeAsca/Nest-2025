9114cc06582c0a8837a12527b42724bd774da365
jorgeasca <jascola171@gamil.com>
2025-10-26 12:01:53 +0100

    Dase de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,12 @@ import { UsuariosModule, } from './usuarios/usuarios.module.js';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // NO USAR EN PRODUCCION
     }),
     UsuariosModule],
   controllers: [AppController],
